4efd37ca93822e33217f6d26fcd5f91596e73a1d
dml92 <Diego@MacBook-Air-de-Diego.local>
2017-01-08 01:41:13 +0100

    Form validation
    Validate user input for new product/order forms.

--- src/pages/new-order/new-order.component.ts
+++ src/pages/new-order/new-order.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Order } from '../../app/order';
 import { OrderService } from '../../app/order.service';
@@ -11,11 +12,24 @@ import { OrderService } from '../../app/order.service';
 export class NewOrderComponent {
 
   orderName : string;
+  newOrderForm : FormGroup;
 
   constructor(
-  	public navCtrl: NavController,
-  	public navParams: NavParams,
-    private orderService: OrderService) {}
+  	private navCtrl: NavController,
+  	private navParams: NavParams,
+    private orderService: OrderService,
+    private formBuilder: FormBuilder
+  ) {
+    this.newOrderForm = this.formBuilder.group({
+      name : [
+        '',
+        Validators.compose([
+          Validators.maxLength(50), 
+          Validators.pattern('[a-zA-ZñÑ0-9][a-zA-ZñÑ 0-9]*'),
+          Validators.required
+        ])      ]
+    });
+  }
 
   onSubmit () {
     //TO-DO: Asynchronous validation.
--- src/pages/new-product/new-product.component.ts
+++ src/pages/new-product/new-product.component.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { Product } from '../../app/product';
 import { ProductService } from '../../app/product.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: 'new-product.component.html'
@@ -12,17 +13,38 @@ export class NewProductComponent {
 
   productName : string;
   productPrice: number;
+  newProductForm: FormGroup;
 
   constructor(
   	private navCtrl: NavController,
   	private navParams: NavParams,
-  	private productService: ProductService) {}
+  	private productService: ProductService,
+    private formBuilder: FormBuilder
+  ) {
+    this.newProductForm = this.formBuilder.group({
+      name: [
+        '',
+        Validators.compose([
+          Validators.maxLength(50), 
+          Validators.pattern('[a-zA-ZñÑ][a-zA-ZñÑ ]*'),
+          Validators.required
+        ])
+      ],
+      price: [
+        '',
+        Validators.compose([
+          Validators.pattern('[0-9]+([.,][0-9]+)?'),
+          Validators.required
+        ])
+      ]
+    });
+  }
 
   onSubmit () {
-    //TO-DO: Asynchronous validation.
-    let product = new Product(this.productName, this.productPrice);
-    this.productService.addProduct(product);
-    this.navCtrl.popToRoot();
+      //TO-DO: Asynchronous validation.
+      let product = new Product(this.productName, this.productPrice);
+      this.productService.addProduct(product);
+      this.navCtrl.popToRoot();
   }
 
 }
