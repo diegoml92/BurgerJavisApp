6b2e69c516267fe940724a6c2d06fb67f7bca3a6
dml92 <Diego@MacBook-Air-de-Diego.local>
2017-01-11 20:23:21 +0100

    Ingredients management
    Now ingredients can be created, modified and deleted. When adding
    ingredients to a product, a checkbox list is presented instead of a
    text input

---
+++ src/pages/ingredients/ingredients.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
+
+import { Ingredient } from '../../app/ingredient';
+import { IngredientService } from '../../app/ingredient.service';
+import { IngredientDetailsComponent } from 
+  '../ingredient-details/ingredient-details.component';
+
+@Component({
+  templateUrl: 'ingredients.component.html'
+})
+export class IngredientsComponent {
+
+  ingredients: Ingredient[];
+
+  constructor(
+    private loadingCtrl: LoadingController,
+    private ingredientService: IngredientService,
+    private navCtrl: NavController,
+    private navParams: NavParams) {}
+
+  ionViewWillEnter() {
+    let loading = this.loadingCtrl.create({
+      content: "Cargando ingredientes..."
+    });
+    loading.present();
+    this.ingredientService.getIngredientList()
+      .then(ingredients => {
+        this.ingredients = ingredients;
+        loading.dismiss();
+      });
+  }
+
+  ingredientTapped(ingredient: Ingredient) {
+    this.navCtrl.push(IngredientDetailsComponent, {
+      ingredient: ingredient
+    });
+  }
+
+}
